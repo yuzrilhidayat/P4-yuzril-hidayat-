d14dd2703f8c570e70e864a190781ccdfc7736bc
yuzrilhidayat <64572075+yuzrilhidayat@users.noreply.github.com>
2020-05-13 02:45:59 -0700

    Create jobsheet 6 C.cpp
    membuat matriks transpose bersifat dinamis menggunakan persoalan diatas

---
+++ jobsheet 6 C.cpp
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+main()
+{
+int i,j;
+int matrik1[2][2];
+printf("NAMA : YUZRIL HIDAYAT \nNIM : F1B019145 \nKELOMPOK : 28\n\n\n");
+for(i=0;i<3;i++)
+{
+for(j=0;j<3;j++)
+{
+printf("Masukkan nilai matrik %i,%i = ",i,j);
+scanf("%i",&matrik1[i][j]);
+}
+}
+for(i=0;i<3;i++)
+{
+for(j=0;j<3;j++)
+{
+printf("%i ",matrik1[i][j]);
+}
+printf("\n");
+}
+printf("nilai matrik setelah di transpose = ");
+printf("\n");
+for(i=0;i<3;i++)
+{
+for(j=0;j<3;j++)
+{
+printf("%i ",matrik1[j][i]);
+}
+printf("\n");
+}
+}
