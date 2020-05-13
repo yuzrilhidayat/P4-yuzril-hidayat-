097a1096da66edca4a5355fa71abe477d948e4e1
yuzrilhidayat <64572075+yuzrilhidayat@users.noreply.github.com>
2020-05-13 02:34:12 -0700

    Create jobsheet 1 C++.cpp
    membuat program array satu dimensi yang menampilkan NIM masing-masing mahasiswa

---
+++ jobsheet 1 C++.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+main(){
+    printf("Nama : YUZRIL HIDAYAT\n");
+    printf("NIM : F1B019145\n");
+    printf("Kelompok : 28\n\n");
+    string my_array[5] = {"F1B018024","F1B018062","F1B01833","F1B018044","F1B018040","F1B018055","F1B019147"};
+    printf("Data ke-1 = %d\n",my_array[0]);
+    printf("Data ke-2 = %d\n",my_array[1]);
+    printf("Data ke-3 = %d\n",my_array[2]);
+    printf("Data ke-4 = %d\n",my_array[3]);
+    printf("Data ke-5 = %d\n",my_array[4]);
+}
